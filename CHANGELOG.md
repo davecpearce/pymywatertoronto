fa2108fd3cdb39e8d22de29e71203a2d840d0360
Dave Pearce <pearcd2@yahoo.com>
2022-08-28 08:35:31 -0400

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+This document will contain a list of all major changes.
+
+## [1.0.0] - 2022-08-28
+
+### Added
+
+- Initial commit
---
+++ tests/test_template.py
@@ -0,0 +1,91 @@
+import asyncio
+
+from datetime import timedelta
+from aiohttp import ClientSession
+import logging
+
+from pymywatertoronto.mywatertoronto import (
+    MyWaterToronto, 
+)
+from pymywatertoronto.const import (
+    KEY_ADDRESS,
+    KEY_METER_FIRST_READ_DATE,
+    KEY_METER_LIST,
+    KEY_METER_NUMBER,
+    KEY_PREMISE_ID,
+    KEY_PREMISE_LIST, 
+)
+from pymywatertoronto.enums import (
+    ConsumptionBuckets,
+    LastPaymentMethod, 
+)
+from pymywatertoronto.errors import (
+    AccountDetailsError,
+    ApiError,
+    ValidateAccountInfoError,
+)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("tests/debug.log",mode='w'),
+        logging.StreamHandler()
+    ]
+)
+
+# Update this section with your City of Toronto Utility account information
+account_number="000000000"
+client_number="000000000-00"
+last_name="lastname"
+postal_code="X1X 1X1"
+last_payment_method=LastPaymentMethod.BANK_PAYMENT
+
+async def main():
+    session = ClientSession()
+
+    mywatertoronto = MyWaterToronto(session,
+                                    account_number, 
+                                    client_number, 
+                                    last_name, 
+                                    postal_code, 
+                                    last_payment_method )
+
+    try:
+        await mywatertoronto.async_validate_account()
+    except ( ValidateAccountInfoError, ApiError ) as err:
+        logging.debug(f"Error validating account with MyWaterToronto API: {err}")
+
+    try:
+        account_details = await mywatertoronto.async_get_account_details()
+    except ( AccountDetailsError, ApiError ) as err:
+        logging.debug(f"Error getting account details from MyWaterToronto API: {err}")
+
+    try:
+        consumption_data = await mywatertoronto.async_get_consumption()
+    except ( ApiError ) as err:
+        logging.debug(f"Error getting water consumption data from MyWaterToronto API: {err}")
+
+    for premise in account_details[KEY_PREMISE_LIST]:
+        premise_id = premise[KEY_PREMISE_ID]
+        premise_address = premise[KEY_ADDRESS]
+        logging.debug('Premise Address: %s', premise_address)  # noqa: WPS323
+
+        meter_list = premise[KEY_METER_LIST]
+        for meter in meter_list:
+            meter_number = meter[KEY_METER_NUMBER]
+            meter_name = f"{premise_address} {meter_number}"
+            logging.debug('Meter Name: %s', meter_name)  # noqa: WPS323
+
+            data = consumption_data[KEY_PREMISE_LIST][premise_id][KEY_METER_LIST][meter_number]
+            firstReadDate = data[KEY_METER_FIRST_READ_DATE]
+            logging.debug('First Read Date: %s', firstReadDate)  # noqa: WPS323
+
+            for bucket in ConsumptionBuckets:
+                consumption = data['consumption_data'][bucket.value]['consumption']
+                unit = data['consumption_data'][bucket.value]['unit_of_measure']
+                logging.debug('%s: %s %s', bucket.value, consumption, unit)  # noqa: WPS323
+
+    await session.close()
+
+asyncio.run(main())
